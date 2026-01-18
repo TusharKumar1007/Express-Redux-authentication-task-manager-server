3d74f2d677396ccd760a74d0a88e421d3fcae8ec
Tusharkumar-dev <githubtk1007@gmail.com>
2026-01-18 21:40:23 +0530

    improved cookie

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,31 +1,21 @@
-// export const addCookie = (res, token) => {
-//   res.cookie("token", token, {
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === "production",
-//     sameSite: "strict",
-//     maxAge: 2000 * 60 * 60 * 24,
-//   });
-// };
-// export const clearCookie = (res) => {
-//   res.clearCookie("token", {
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === "production",
-//     sameSite: "strict",
-//   });
-// };
+import dotenv from "dotenv";
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const addCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
-    maxAge: 1500 * 60 * 60 * 24,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "strict",
+    maxAge: isProduction ? 1500 * 60 * 60 * 24 : 2000 * 60 * 60 * 24,
   });
 };
+
 export const clearCookie = (res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "strict",
   });
 };
